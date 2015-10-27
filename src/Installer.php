8868e613f02f6b766b802092ce84d2f328671298
Nejtr0n <a6y@xakep.ru>
2015-10-27 11:21:13 +0300

    commit

--- src/Installer.php
+++ src/Installer.php
@@ -1,9 +1,11 @@
 <?php
 namespace Nejtr0n/Bx_gitignore;
-use ComposerScriptEvent;
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
 
 class Installer {
   public static function copy(Event $event){
     $event->getIO()->write("Show me after INSTALL/UPDATE command");
+    var_dump($event);
   }
 }
